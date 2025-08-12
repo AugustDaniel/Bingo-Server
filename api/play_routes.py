6ef8d261aa81bee05ecd6e53e2d991be85100c50
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-13 00:49:05 +0200

    added some template code for websocket

--- api/play_routes.py
+++ api/play_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 router = APIRouter(
     prefix="/play",
@@ -6,7 +6,36 @@ router = APIRouter(
 )
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @router.websocket("/ws/{room_id}")
-async def websocket_endpoint(room_id: str, player_name: str, websocket: WebSocket):
-    await websocket.accept()
-    pass
+async def websocket_endpoint(websocket: WebSocket, room_id: str, player_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{player_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{player_id} left the chat")
