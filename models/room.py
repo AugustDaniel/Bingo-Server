9361abf6e67bc8ef355d847f9d8d57078b44d0ea
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-12 23:27:23 +0200

    made fiedl in room model optional

--- models/room.py
+++ models/room.py
@@ -5,7 +5,7 @@ class RoomModel(BaseModel):
     room_id: str = Field(..., examples=["1112"])
     name: str = Field(..., examples=["Room1", "Room2"])
     capacity: int = Field(..., examples=[20])
-    is_full: bool = Field(..., examples=[True, False])
+    is_full: bool = Field(default=False, examples=[True, False])
 
 
 class RoomJoinResponse(BaseModel):
