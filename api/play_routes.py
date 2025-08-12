d3889ef7143f53750663707d631418474dc9046d
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-12 13:12:10 +0200

    add websocket route

--- api/play_routes.py
+++ api/play_routes.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
 
-router = APIRouter()
+router = APIRouter(
+    prefix="play",
+    tags=["play"]
+)
+
+
+@router.websocket("/ws/{room_id}")
+async def websocket_endpoint(room_id: str, player_name: str, websocket: WebSocket):
+    await websocket.accept()
+    pass
