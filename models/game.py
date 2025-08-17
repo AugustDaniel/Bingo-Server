8352ddb3bfd365635ea315ab83ead9da3237da24
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-17 17:30:31 +0200

    added comment for clarification to model

--- models/game.py
+++ models/game.py
@@ -6,6 +6,6 @@ from models import RoomModel
 
 
 class GameModel(BaseModel):
-    rooms: Dict[str, RoomModel] = Field(...)
+    rooms: Dict[str, RoomModel] = Field(...) # room_id -> RoomModel
     max_capacity: int = Field(..., examples=[20])
     max_rooms: int = Field(..., examples=[20])
