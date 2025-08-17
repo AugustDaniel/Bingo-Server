a4f52bb5975ba6d9f04283ed057be7d68670f5e2
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-17 19:01:48 +0200

    added connect for websocket route

--- api/play_routes.py
+++ api/play_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 from dependencies import get_play_service
+from services import PlayService
 
 router = APIRouter(
     prefix="/play",
@@ -8,15 +9,14 @@ router = APIRouter(
 
 
 @router.websocket("/ws/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: str, player_id: str, service=Depends(get_play_service)):
-    service
+async def websocket_endpoint(websocket: WebSocket, room_id: str, player_id: str,
+                             service: PlayService = Depends(get_play_service)):
+    await service.connect(websocket, room_id, player_id)
 
-    await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{player_id} says: {data}")
+            data = await websocket.receive_json()
+            # service.handle_message(data)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{player_id} left the chat")
+        pass
+        # service.disconnect(websocket)
