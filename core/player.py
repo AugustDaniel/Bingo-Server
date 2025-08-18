a3d4c787e435f8a00767ad93af066bd9ba92c01e
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-18 22:49:55 +0200

    added bingocard to player

--- core/player.py
+++ core/player.py
@@ -1,4 +1,8 @@
+from bingo_card import BingoCard
+
+
 class Player:
     def __init__(self, name: str, player_id: str):
         self.id: str = player_id
         self.name: str = name
+        self.cards: list[BingoCard] = []
