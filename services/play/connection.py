4c94f7dd660a6cbd75d0a377bdc9a6c88763fd50
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-21 01:24:17 +0200

    extracted play service into its own module

---
+++ services/play/connection.py
@@ -0,0 +1,52 @@
+import asyncio
+
+from fastapi import WebSocket
+
+from core import Player, Room
+from models import WebSocketMessage
+
+
+class Connection:
+    def __init__(self, player: Player, room: Room, websocket: WebSocket):
+        self.player: Player = player
+        self.room: Room = room
+        self.__websocket: WebSocket = websocket
+
+    async def send(self, message: WebSocketMessage):
+        await self.__websocket.send_json(message)
+
+    async def receive(self) -> WebSocketMessage:
+        data = await self.__websocket.receive_json()
+        return WebSocketMessage(**data)
+
+    async def close(self):
+        await self.__websocket.close()
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.__connections: list[Connection] = []
+        self.__lock = asyncio.Lock()
+
+    async def add_connection(self, connection: Connection):
+        async with self.__lock:
+            self.__connections.append(connection)
+
+    async def remove_connection(self, connection: Connection):
+        async with self.__lock:
+            try:
+                self.__connections.remove(connection)
+            except ValueError:
+                pass
+
+    async def get_connections(self) -> list[Connection]:
+        async with self.__lock:
+            return list(self.__connections)
+
+    async def broadcast(self, message: WebSocketMessage):
+        connections = await self.get_connections()
+
+        await asyncio.gather(
+            *(c.send(message) for c in connections),
+            return_exceptions=True
+        )
--- services/play/play.py
+++ services/play/play.py
@@ -7,52 +7,7 @@ from core import Game, Room, Player
 from exceptions import InvalidWebSocketJoin
 from mapper import map_bingo_card_to_response
 from models.websocket import *
-
-
-class Connection:
-    def __init__(self, player: Player, room: Room, websocket: WebSocket):
-        self.player: Player = player
-        self.room: Room = room
-        self.__websocket: WebSocket = websocket
-
-    async def send(self, message: WebSocketMessage):
-        await self.__websocket.send_json(message)
-
-    async def receive(self) -> WebSocketMessage:
-        data = await self.__websocket.receive_json()
-        return WebSocketMessage(**data)
-
-    async def close(self):
-        await self.__websocket.close()
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.__connections: list[Connection] = []
-        self.__lock = asyncio.Lock()
-
-    async def add_connection(self, connection: Connection):
-        async with self.__lock:
-            self.__connections.append(connection)
-
-    async def remove_connection(self, connection: Connection):
-        async with self.__lock:
-            try:
-                self.__connections.remove(connection)
-            except ValueError:
-                pass
-
-    async def get_connections(self) -> list[Connection]:
-        async with self.__lock:
-            return list(self.__connections)
-
-    async def broadcast(self, message: WebSocketMessage):
-        connections = await self.get_connections()
-
-        await asyncio.gather(
-            *(c.send(message) for c in connections),
-            return_exceptions=True
-        )
+from .connection import ConnectionManager, Connection
 
 
 class PlayService:
